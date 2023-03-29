6c27094ca49f8de76719c6408c857706c8eb9c50
Laurie Fish <iamlauriefish@gmail.com>
2023-03-28 18:31:43 -0700

    functions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment code here
-
-
+function specialCheck(_string) {
+const special = !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+for (var i = 0; i < _string.length; i++) {
+ if ??????
+}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +19,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+if (password.value.length > 8 && password.value < 120 && *Sp.char.*) {
+  msg="success"
+  alert(msg);
+}else if(password.value.length < 8) {
+  msg="password needs to be more than 8 characters."
+  alert(msg); 
+}
+ else if (password.value.length > 120) {
+  msg="You can't have more than 128 characters"
+  alert(msg);
+}
+/*else if (special characters) {
+  alert: you need at least 2 special characters.
+}*/
