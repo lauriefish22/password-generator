d2b1051e977456716a91b9a60c875ff10260629f
Laurie Fish <iamlauriefish@gmail.com>
2023-03-29 09:29:17 -0700

    fixed function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-const special = !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,30 +9,34 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  const special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
   passwordText.value = password;
 
+
+  if(!passwordText.value.length < 8 || !password.value.length >128) {
+    msg="password needs to be more than 8 characters and less than 128"
+    alert(msg); 
+  
+  } else if (passwordText.value.length < 128 && password.value.length >=8) {
+    msg="success"
+    alert(msg);
+  
+  } else if (passwordText.value.length < 1 (special)) {
+    msg="You need at at least one special character"
+    alert(msg);
+  
+  } else (passwordText.value.length >= 8 && password.value.length < 128 && passwordText.value.length > 1(special)) {
+    msg="success"
+    alert(msg);
+  }
+  
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
-if(!passwordText.value.length < 8 || !password.value.length >128) {
-  msg="password needs to be more than 8 characters and less than 128"
-  alert(msg); 
-
-} else if (passwordText.value.length < 128 && password.value.length >=8) {
-  msg="success"
-  alert(msg);
-
-} else if (passwordText.value.length < 1 (special)) {
-  msg="You need at at least one special character"
-  alert(msg);
-
-} else (passwordText.value.length >= 8 && password.value.length < 128 && passwordText.value.length > 1(special)) {
-  msg="success"
-  alert(msg);
-}
 
