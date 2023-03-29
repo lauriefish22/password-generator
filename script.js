c5681e34e335562bee47f604fb806e727738ee19
Laurie Fish <iamlauriefish@gmail.com>
2023-03-29 16:26:57 -0700

    added function generatePassword

--- script.js
+++ script.js
@@ -9,12 +9,32 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  const special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  
 
   passwordText.value = password;
+}
+function generatePassword() {
+    let charLength = window.prompt("Enter in your desired number of characters");
+  if (charLength < 8 || charLength > 128) {
+    window.alert("Password must be between 8 and 128 characters.")
+  } else {
+    let lowerCase = window.confirm("Would you like lowercase letters in your password?"); 
+    let upperCase = window.confirm("Would you like uppercase letters in your password?");
+    let specChar = window.confirm("Would you like to include special characters?");
+    let digits = window.confirm("Would you like to include numeric values?");
+    
+  
+  }
 
 
-  if(!passwordText.value.length < 8 || !password.value.length >128) {
+}
+
+
+
+
+
+
+  /*if(!passwordText.value.length < 8 || !password.value.length >128) {
     msg="password needs to be more than 8 characters and less than 128"
     alert(msg); 
   
@@ -31,8 +51,8 @@ function writePassword() {
     alert(msg);
   }
   
-}
-
+}*/
+// const special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
